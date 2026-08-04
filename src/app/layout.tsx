d2e2090139fffbef3d98a5472b14ef8a46e761a0
yexe <y.exe.1201@proton.me>
2026-08-04 22:26:37 +0900

    feat: OGP設定を追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,30 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "やまかわ動画編集大会",
-  description: "やまかわ動画編集大会の投票サイト",
+  description: "やまかわ動画編集大会の投票サイト。あなたのお気に入りの作品に投票しよう！",
+  metadataBase: new URL("https://event.ymkw.top"),
+  openGraph: {
+    title: "やまかわ動画編集大会",
+    description: "やまかわ動画編集大会の投票サイト。あなたのお気に入りの作品に投票しよう！",
+    url: "https://event.ymkw.top",
+    siteName: "やまかわ動画編集大会",
+    images: [
+      {
+        url: "/ogp/ogp.png",
+        width: 1200,
+        height: 630,
+        alt: "やまかわ動画編集大会",
+      },
+    ],
+    locale: "ja_JP",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "やまかわ動画編集大会",
+    description: "やまかわ動画編集大会の投票サイト。あなたのお気に入りの作品に投票しよう！",
+    images: ["/ogp/ogp.png"],
+  },
 };
 
 export default function RootLayout({
